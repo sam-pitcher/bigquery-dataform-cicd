2d9bd72c92a55cbd08c17c8f72d7fba6d8bd145b
Sam Pitcher <sampitcher@google.com>
2024-09-25 21:28:57 +0200

    update

--- run_dataform.sh
+++ run_dataform.sh
@@ -3,8 +3,8 @@
 # set -eo pipefail
 
 echo "{\"projectId\": \"${PROJECT_ID}\", \"location\": \"${LOCATION}\"}" > .df-credentials.json
-# cp "${BRANCH_NAME}_dataform.json" dataform.json
+cp "${BASE_BRANCH}_workflow_settings.yaml" workflow_settings.yaml
 
 
 npm i -g @dataform/cli@^3.0.0-beta
-dataform run --vars=datasetSuffix=${BASE_BRANCH}
+dataform run
